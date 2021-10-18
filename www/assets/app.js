4822f4eee6a5c470609a3ba1f357d9d5b1e910ef
es-heisst-parkhaus <92606696+es-heisst-parkhaus@users.noreply.github.com>
2021-10-18 10:28:30 +0000

    Add files via upload

---
+++ www/assets/app.js
@@ -0,0 +1,35 @@
+
+(function(){
+
+	function init(){
+		loadItemList();
+	}
+
+	function loadItemList(){
+		let itemEl = document.getElementsByTagName("main")[0];
+		if(!itemEl)
+			throw new Error("Element 'main' not found");
+		itemEl.innerHTML = "LOADING";
+		fetch("items.json").then((res) => {
+			return res.json();
+		}).then((json) => {
+			let innerHTML = "";
+			for(let i of json){
+				innerHTML += '<div class="itemcard"><img src="assets/items/' + i.id + '.png" /><div class="title">' + i.name + '</div><div class="description">' + i.description + '</div>'
+				 + '<div class="stars" style="--rating: ' + i.rating + '"></div><span class="ratingCount">(' + i.ratingCount + ')</span>';
+				if(i.inStock > 0)
+					innerHTML += '<div class="inStock">' + i.inStock + ' in stock</div>';
+				else
+					innerHTML += '<div class="inStock outOfStock">Out of stock</div>';
+				innerHTML += '</div>';
+			}
+			itemEl.innerHTML = innerHTML;
+		});
+	}
+
+
+	window.addEventListener("DOMContentLoaded", init);
+
+})();
+
+
