1b654f0fdcf138ff7013fe1fa22a307361ce4e0f
ZY4N <admin@zy4n.com>
2021-10-23 02:26:18 +0200

    added ugly confetti

--- www/assets/app.js
+++ www/assets/app.js
@@ -2,21 +2,23 @@
 (function(){
 
 	function init(){
-		loadItemList();
+		loadItemList(addBuyPopup);
 	}
 
-	function loadItemList(){
+	function loadItemList(onFinished){
 		let itemEl = document.getElementsByTagName("main")[0];
 		if(!itemEl)
 			throw new Error("Element 'main' not found");
 		itemEl.innerHTML = "LOADING";
+
+		
 		fetch("items.json").then((res) => {
 			return res.json();
 		}).then((json) => {
 			let innerHTML = "";
 			for(let i of json){
 				innerHTML += '<div class="itemcard"><img src="assets/items/' + i.id + '.png" /><div class="title">' + i.name + '</div><div class="description">' + i.description + '</div>'
-				 + '<div class="stars" style="--rating: ' + i.rating + '"></div><span class="ratingCount">(' + i.ratingCount + ')</span>';
+					+ '<div class="stars" style="--rating: ' + i.rating + '"></div><span class="ratingCount">(' + i.ratingCount + ')</span>';
 				if(i.inStock > 0)
 					innerHTML += '<div class="inStock">' + i.inStock + ' in stock</div>';
 				else
@@ -25,9 +27,24 @@
 				innerHTML += '</div>';
 			}
 			itemEl.innerHTML = innerHTML;
+			onFinished();
 		});
 	}
 
+	function addBuyPopup() {
+		let background = document.getElementById("popupBackground");
+		let buttons = document.getElementsByClassName("buttonFront");
+		for(let button of buttons) {
+			button.addEventListener("click", () => {
+				background.style.display = "block";
+				background.style["background-image"] = "url(\"assets/items/confetti.png\")";
+				background.innerHTML = "<div id=\"popup\" class=\"popup\"><b>Added to cart!</b></div>";
+				background.addEventListener("click", event => {
+					background.style.display = "none";
+				});
+			});
+		}
+	}
 
 	window.addEventListener("DOMContentLoaded", init);
 
