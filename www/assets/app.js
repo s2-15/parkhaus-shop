71947a384d1da95846e50358bc49b9452e2d8bb4
ZY4N <admin@zy4n.com>
2022-04-15 23:00:12 +0200

    added confetti

--- www/assets/app.js
+++ www/assets/app.js
@@ -51,6 +51,7 @@
 		};
 		for (let button of buttons) {
 			button.addEventListener("click", () => {
+				startConfetti();
 				background.style.display = "block";
 				background.style["background-image"] = "url(\"assets/items/confetti.png\")";
 				background.innerHTML = "<div id=\"popup\" class=\"popup\"><b>Added to cart!</b></div>";
@@ -58,6 +59,27 @@
 				hidePopupEvent = setTimeout(hidePopup, 2000);
 			});
 		}
+	}
+	
+	function startConfetti() {
+		let duration = 5 * 1000;
+		let animationEnd = Date.now() + duration;
+		let defaults = { startVelocity: 30, spread: 360, ticks: 60, zIndex: 0 };
+
+		function randomInRange(min, max) {
+			return Math.random() * (max - min) + min;
+		}
+
+		let interval = setInterval(function() {
+		let timeLeft = animationEnd - Date.now();
+
+		if (timeLeft <= 0) {
+			return clearInterval(interval);
+		}
+		let particleCount = 200 * (timeLeft / duration);
+		confetti(Object.assign({}, defaults, { particleCount, origin: { x: randomInRange(0.0, 0.9), y: Math.random() - 0.2 } }));
+		confetti(Object.assign({}, defaults, { particleCount, origin: { x: randomInRange(0.0, 0.9), y: Math.random() - 0.1 } }));
+		}, 250);
 	}
 
 	window.addEventListener("DOMContentLoaded", init);
